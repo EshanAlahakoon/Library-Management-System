619e8606a3551ad01a2bad79bee7c641e29f08ab
Eshan Alahakoon <eshanmax427xp@gmail.com>
2026-01-13 12:26:42 +0530

    Add files via upload

---
+++ test/Test.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package test;
+
+/**
+ *
+ * @author Eshan
+ */
+public class Test {
+    public static void main(String[] args){
+        System.out.println("first");
+    }
+}
